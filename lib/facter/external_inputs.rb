63c8948c8caf1d52b89c7cd8c968ea58f2b1609a
kaazoo <kaazoo@drqueue.org>
2013-09-23 13:23:23 +0200

    remove username and password facts

--- lib/facter/external_inputs.rb
+++ lib/facter/external_inputs.rb
@@ -3,15 +3,3 @@ Facter.add("oracle_license_accepted") do
     ENV['I_ACCEPT_THE_ORACLE_LICENSE'] ? true : false
   end
 end
-
-Facter.add("neo4j_username") do
-  setcode do
-    ENV['NEO4J_USERNAME'] ||= 'neo4j'
-  end
-end
-
-Facter.add("neo4j_password") do
-  setcode do
-    ENV['NEO4J_PASSWORD'] || 'Never.Use.Defaults'
-  end
-end
