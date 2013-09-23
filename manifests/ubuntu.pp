e523259f5db14dc5eac14172299034c278a62cf8
kaazoo <kaazoo@drqueue.org>
2013-09-23 13:09:30 +0200

    update only when needed fix grep use new jar file fix variable names remove redundant service handler

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -30,32 +30,25 @@ class neo4j::ubuntu {
 
   exec {
     'apt-get update':
-      command => '/usr/bin/apt-get update';
+      command     => '/usr/bin/apt-get update',
+      refreshonly => true;
 
     'neotech signing key':
       command => '/usr/bin/wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add -',
       before  => Exec['apt-get update'],
       unless  => '/usr/bin/apt-key list | grep -q neotechnology.com';
 
-    'restart neo4j':
-      command     => '/usr/sbin/service neo4j-service restart',
-      require     => [Package['neo4j'],
-                      Class['neo4j::linux'],
-                      File['neo4j config file'],
-                      File['neo4j auth extension link'],
-                      Exec['bump the minimum heap size'],
-                      Exec['bump the maximum heap size'],
-      ];
-
     'bump the minimum heap size':
       command     => '/bin/echo "wrapper.java.initmemory=1024" >> /etc/neo4j/neo4j-wrapper.conf',
       require     => Package['neo4j'],
-      unless      => '/bin/grep -q "^wrapper.java.initmemory"';
+      unless      => '/bin/grep -q "^wrapper.java.initmemory" /etc/neo4j/neo4j-wrapper.conf',
+      notify      => Service['neo4j-service'];
 
     'bump the maximum heap size':
       command     => '/bin/echo "wrapper.java.maxmemory=4096" >> /etc/neo4j/neo4j-wrapper.conf',
       require     => Package['neo4j'],
-      unless      => '/bin/grep -q "^wrapper.java.maxmemory"';
+      unless      => '/bin/grep -q "^wrapper.java.maxmemory" /etc/neo4j/neo4j-wrapper.conf',
+      notify      => Service['neo4j-service'];
   }
 
   file {
@@ -64,26 +57,27 @@ class neo4j::ubuntu {
       path   => '/usr/share/neo4j-contrib';
 
     'neo4j auth extension':
-      path    => '/usr/share/neo4j-contrib/authentication-extension-1.8.1-fudge.jar',
-      source  => 'puppet:///modules/neo4j/authentication-extension-1.8.1-fudge.jar',
+      path    => '/usr/share/neo4j-contrib/authentication-extension-2.0.0-M04-1.0-SNAPSHOT.jar',
+      source  => 'puppet:///modules/neo4j/authentication-extension-2.0.0-M04-1.0-SNAPSHOT.jar',
       require => File['neo4j contrib dir'];
 
     'neo4j auth extension link':
       ensure  => 'link',
-      path    =>  '/usr/share/neo4j/plugins/authentication-extension-1.8.1-fudge.jar',
-      target  => '/usr/share/neo4j-contrib/authentication-extension-1.8.1-fudge.jar',
-      require => File['neo4j auth extension'];
+      path    =>  '/usr/share/neo4j/plugins/authentication-extension-2.0.0-M04-1.0-SNAPSHOT.jar',
+      target  => '/usr/share/neo4j-contrib/authentication-extension-2.0.0-M04-1.0-SNAPSHOT.jar',
+      require => File['neo4j auth extension'],
+      notify  => Service['neo4j-service'];
 
     'neo4j apt config':
       path    => '/etc/apt/sources.list.d/neo4j.list',
-      content => 'deb http://debian.neo4j.org/repo stable/',
-      before  => Exec['apt-get update'];
+      content => $neo4j::params::neo4j_debian_repository,
+      notify  => Exec['apt-get update'];
 
     'neo4j config file':
       path     => '/etc/neo4j/neo4j-server.properties',
       content  => template('neo4j/neo4j-server.properties.erb'),
       require  => Package['neo4j'],
-      owner    => neo4j,
+      owner    => $neo4j::neo4j_username,
       group    => adm;
   }
 
@@ -97,7 +91,12 @@ class neo4j::ubuntu {
     'neo4j-service':
       ensure  => running,
       enable  => true,
-      require => Exec['restart neo4j'],
+      require => [Package['neo4j'],
+                Class['neo4j::linux'],
+                File['neo4j config file'],
+                File['neo4j auth extension link'],
+                Exec['bump the minimum heap size'],
+                Exec['bump the maximum heap size']],
       subscribe => File['neo4j config file'];
   }
 
